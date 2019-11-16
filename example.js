b31db9d91cbf0a54299045f435e21de9cb4069b7
Erik Gardea <erik1gardea@gmail.com>
2019-11-16 14:47:44 -0600

    functions, @ts-check

---
+++ example.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+//implicit type
+var example = [1, 2, 3, 4];
+//explicit type
+var ex2 = [1, 2, 3, 4, true];
+//functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(1, 2));
--- example.ts
+++ example.ts
@@ -1,4 +1,10 @@
 //implicit type
 const example = [1, 2, 3, 4];
 //explicit type
-const ex2: number[] = [1, 2, 3, 4];
+const ex2: (number | boolean)[] = [1, 2, 3, 4, true];
+
+//functions
+function add(num1: number, num2: number): number{
+    return num1 + num2;
+}
+console.log(add(1, 2));
