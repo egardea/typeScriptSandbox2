75d98ee0cca4c6fecead5d3c1f2a370ed63a6c82
Erik Gardea <erik1gardea@gmail.com>
2019-11-17 20:34:12 -0600

    classes

---
+++ example.class.ts
@@ -0,0 +1,16 @@
+export class PersonClass {
+    public readonly firstName: string;
+    public readonly lastName: string;
+    public age: boolean;
+    private bankAccount: number;
+
+    //constructors
+    constructor(data){
+        this.firstName = data.firstName;
+    }
+
+    //functions
+    public example (){
+
+    }
+}
